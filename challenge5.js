1140abe8e2700d686bc8844b4b8e5f58bfcfddfd
Keira Jarvis <keirajarvis@CN0412.local>
2023-10-11 16:50:12 +0100

    added security vulnerabilities

--- challenge5.js
+++ challenge5.js
@@ -2,38 +2,38 @@ let is2FAEnabled = false;
 let isSoftwareUpdated = false;
 let isFirewallActive = false;
 let isVPNActive = false;
-let timerStarted = false;
-
-let timeLeft = 15 * 60; // 15 minutes converted to seconds
+let timeLeft = 15 * 60; // 15 minutes in seconds
 let timerInterval;
 
-function startTimer() {
-    if (timerStarted) return;
-    timerStarted = true;
-
-    const timerElem = document.getElementById('timer');
-    timerElem.style.display = 'block';
-
-    timerInterval = setInterval(function() {
-        if (timeLeft <= 0) {
-            clearInterval(timerInterval);
-            alert('Time\'s up!');
-        } else {
-            let minutes = Math.floor(timeLeft / 60);
-            let seconds = timeLeft % 60;
-            timerElem.innerText = `Time Remaining: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-            timeLeft--;
-        }
-    }, 1000);
-}
-
-function displayHint() {
-    alert('Always ensure you follow the company policy for passwords.');
-    timeLeft -= 120; // Deducting 2 minutes as a penalty for using the hint.
-}
+let randomSecurityIssues = [
+    {
+        message: "Malware detected!",
+        actionRequired: ["updateSoftware"],
+        warning: "A potential malware threat has been detected! Check the company policies for the steps to resolve this issue."
+    },
+    {
+        message: "Suspicious login attempt detected!",
+        actionRequired: ["toggle2FA", "toggleVPN"],
+        warning: "Suspicious login attempts detected! Refer to the company policies for resolution steps."
+    },
+    {
+        message: "Data breach detected! User credentials at risk!",
+        actionRequired: ["toggleFirewall", "toggleVPN"],
+        warning: "A potential data breach detected! Check the company policy for guidance."
+    },
+    {
+        message: "DDoS attack initiated!",
+        actionRequired: ["toggleFirewall"],
+        warning: "DDoS attack detected! What does the company policy suggest?"
+    },
+    {
+        message: "Security certificate expired!",
+        actionRequired: ["updateSoftware", "toggleVPN"],
+        warning: "The security certificate has expired! Ensure you follow the company policy to resolve this."
+    }
+];
 
 function checkPasswordStrength(password) {
-    startTimer();
     const feedback = document.getElementById('passwordFeedback');
 
     if (password.length > 8 && /[0-9]/.test(password) && /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password)) {
@@ -43,7 +43,7 @@ function checkPasswordStrength(password) {
     } else {
         feedback.classList.add('alert');
         feedback.classList.remove('valid');
-        feedback.innerText = "Must be over 8 characters, contain numbers, and special characters";
+        feedback.innerText = "Must be over 8 characters, contain numbers, and special characters.";
     }
 }
 
@@ -69,30 +69,53 @@ function toggleVPN() {
 
 function simulateAttack() {
     const feedback = document.getElementById('simulationFeedback');
-    
-    if(is2FAEnabled && isSoftwareUpdated && isFirewallActive && isVPNActive) {
+
+    if (is2FAEnabled && isSoftwareUpdated && isFirewallActive && isVPNActive) {
         feedback.classList.add('valid');
         feedback.innerText = "Your system is secure! Good job!";
     } else {
         feedback.classList.add('alert');
         let missingSecurityMeasures = [];
 
-        if(!is2FAEnabled) missingSecurityMeasures.push('2FA');
-        if(!isSoftwareUpdated) missingSecurityMeasures.push('Software Updates');
-        if(!isFirewallActive) missingSecurityMeasures.push('Firewall');
-        if(!isVPNActive) missingSecurityMeasures.push('VPN');
+        if (!is2FAEnabled) missingSecurityMeasures.push('2FA');
+        if (!isSoftwareUpdated) missingSecurityMeasures.push('Software Updates');
+        if (!isFirewallActive) missingSecurityMeasures.push('Firewall');
+        if (!isVPNActive) missingSecurityMeasures.push('VPN');
 
         feedback.innerText = `Your system is vulnerable! Activate: ${missingSecurityMeasures.join(', ')}`;
     }
+
+    penalizeUser(); // Call the penalizeUser function after the attack simulation
 }
 
-const simulateAttackBtn = document.getElementById('simulateAttack');
-const hintBtn = document.getElementById('hint');
-
-if (simulateAttackBtn) {
-    simulateAttackBtn.addEventListener('click', simulateAttack);
+function penalizeUser() {
+    if (!window[randomIssue.actionRequired]) {
+        timeLeft -= 120;  // Deduct 2 minutes
+    }
 }
 
-if (hintBtn) {
-    hintBtn.addEventListener('click', displayHint);
+function openRandomSecurityAlert() {
+    let randomIssue = randomSecurityIssues[Math.floor(Math.random() * randomSecurityIssues.length)];
+    alert(`${randomIssue.message} ${randomIssue.warning}`);
 }
+
+function startTimer() {
+    const timerElem = document.getElementById('timer');
+    timerInterval = setInterval(function() {
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            alert("Time's up!");
+        } else {
+            let minutes = Math.floor(timeLeft / 60);
+            let seconds = timeLeft % 60;
+            timerElem.innerText = `Time Remaining: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+            timeLeft--;
+        }
+    }, 1000);
+}
+
+document.getElementById('simulateAttack').addEventListener('click', simulateAttack);
+document.addEventListener("DOMContentLoaded", function() {
+    openRandomSecurityAlert(); // Call this function to display a random security alert when the page loads
+    startTimer();
+});
