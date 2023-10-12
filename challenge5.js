840e1f6ad9e56ffe5bedc2c69dbba780bc8270d1
Keira Jarvis <keirajarvis@CN0412.local>
2023-10-12 17:33:52 +0100

    updates and bug fixes

--- challenge5.js
+++ challenge5.js
@@ -2,50 +2,70 @@ let is2FAEnabled = false;
 let isSoftwareUpdated = false;
 let isFirewallActive = false;
 let isVPNActive = false;
-let timeLeft = 15 * 60; // 15 minutes in seconds
+let isPasswordChanged = false;
+let timeLeft = 5 * 60; // 5 minutes in seconds
 let timerInterval;
 
-let randomSecurityIssues = [
+const securityIssues = [
     {
         message: "Malware detected!",
-        actionRequired: ["updateSoftware"],
+        actionRequired: ["updateSoftware", "toggleVPN", "changePassword"],
         warning: "A potential malware threat has been detected! Check the company policies for the steps to resolve this issue."
     },
     {
         message: "Suspicious login attempt detected!",
-        actionRequired: ["toggle2FA", "toggleVPN"],
+        actionRequired: ["toggle2FA", "toggleVPN", "changePassword"],
         warning: "Suspicious login attempts detected! Refer to the company policies for resolution steps."
     },
     {
-        message: "Data breach detected! User credentials at risk!",
-        actionRequired: ["toggleFirewall", "toggleVPN"],
+        message: "Data breach detected! User credentials at risk:",
+        actionRequired: ["toggleFirewall", "toggleVPN", "changePassword"],
         warning: "A potential data breach detected! Check the company policy for guidance."
     },
     {
         message: "DDoS attack initiated!",
-        actionRequired: ["toggleFirewall"],
+        actionRequired: ["toggleFirewall", "changePassword"],
         warning: "DDoS attack detected! What does the company policy suggest?"
     },
     {
         message: "Security certificate expired!",
-        actionRequired: ["updateSoftware", "toggleVPN"],
+        actionRequired: ["updateSoftware", "toggleVPN", "changePassword"],
         warning: "The security certificate has expired! Ensure you follow the company policy to resolve this."
     }
 ];
 
+// Add an event listener to the "Change Password" button
+document.getElementById('changePasswordButton').addEventListener('click', function () {
+    // Perform password strength check
+    const password = prompt('Enter your new password:');
+    checkPasswordStrength(password);
+});
+
 function checkPasswordStrength(password) {
     const feedback = document.getElementById('passwordFeedback');
+    const hasCapital = /[A-Z]/.test(password);
+    const hasThreeNumbers = (password.match(/\d/g) || []).length >= 3;
+    const hasSpecialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password);
 
-    if (password.length > 8 && /[0-9]/.test(password) && /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password)) {
+    if (password.length > 8 && hasCapital && hasThreeNumbers && hasSpecialCharacter) {
         feedback.classList.add('valid');
         feedback.classList.remove('alert');
         feedback.innerText = "Password is strong!";
+        // Password is considered changed when it meets the strength criteria
+        isPasswordChanged = true;
+
+        // Enable the "Change Password" button
+        document.getElementById('changePasswordButton').disabled = false;
     } else {
         feedback.classList.add('alert');
         feedback.classList.remove('valid');
-        feedback.innerText = "Must be over 8 characters, contain numbers, and special characters.";
+        feedback.innerText = "Must be over 8 characters, contain 1 capital letter, at least 3 numbers, and a special character.";
+
+        // Disable the "Change Password" button if the password is not strong
+        document.getElementById('changePasswordButton').disabled = true;
     }
 }
+
 
 function toggle2FA() {
     is2FAEnabled = !is2FAEnabled;
@@ -69,45 +89,77 @@ function toggleVPN() {
 
 function simulateAttack() {
     const feedback = document.getElementById('simulationFeedback');
+    const randomIssue = securityIssues[Math.floor(Math.random() * securityIssues.length)];
 
-    if (is2FAEnabled && isSoftwareUpdated && isFirewallActive && isVPNActive) {
-        feedback.classList.add('valid');
-        feedback.innerText = "Your system is secure! Good job!";
-    } else {
-        feedback.classList.add('alert');
-        let missingSecurityMeasures = [];
+    if (randomIssue) {
+        const requiredActions = randomIssue.actionRequired;
+        const missingActions = [];
 
-        if (!is2FAEnabled) missingSecurityMeasures.push('2FA');
-        if (!isSoftwareUpdated) missingSecurityMeasures.push('Software Updates');
-        if (!isFirewallActive) missingSecurityMeasures.push('Firewall');
-        if (!isVPNActive) missingSecurityMeasures.push('VPN');
+        for (const action of requiredActions) {
+            // Check if the action is relevant to the current policy
+            switch (action) {
+                case "updateSoftware":
+                    if (!isSoftwareUpdated) {
+                        missingActions.push('Update Software');
+                    }
+                    break;
+                case "toggleVPN":
+                    if (!isVPNActive) {
+                        missingActions.push('Activate VPN');
+                    }
+                    break;
+                case "toggle2FA":
+                    if (!is2FAEnabled) {
+                        missingActions.push('Enable Two-Factor Authentication (2FA)');
+                    }
+                    break;
+                case "toggleFirewall":
+                    if (!isFirewallActive) {
+                        missingActions.push('Activate Firewall');
+                    }
+                    break;
+                case "changePassword":
+                    if (!isPasswordChanged) {
+                        missingActions.push('Change Password');
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
 
-        feedback.innerText = `Your system is vulnerable! Activate: ${missingSecurityMeasures.join(', ')}`;
+        if (missingActions.length === 0) {
+            feedback.classList.toggle('valid', true);
+            feedback.innerText = "Your system is secure! Good job!";
+        } else {
+            feedback.classList.toggle('valid', false);
+            feedback.innerText = `Your system is vulnerable! Take action: ${missingActions.join(', ')}`;
+            penalizeUser(); // Call the penalizeUser function after the attack simulation
+        }
     }
-
-    penalizeUser(); // Call the penalizeUser function after the attack simulation
 }
 
+
 function penalizeUser() {
-    if (!window[randomIssue.actionRequired]) {
-        timeLeft -= 120;  // Deduct 2 minutes
-    }
+    timeLeft -= 120; // Deduct 2 minutes
 }
 
 function openRandomSecurityAlert() {
-    let randomIssue = randomSecurityIssues[Math.floor(Math.random() * randomSecurityIssues.length)];
-    alert(`${randomIssue.message} ${randomIssue.warning}`);
+    const randomIssue = securityIssues[Math.floor(Math.random() * securityIssues.length)];
+    
+    // Display a popup message with the issue details
+    alert(`${randomIssue.message}\n\n${randomIssue.warning}`);
 }
 
 function startTimer() {
     const timerElem = document.getElementById('timer');
-    timerInterval = setInterval(function() {
+    timerInterval = setInterval(function () {
         if (timeLeft <= 0) {
             clearInterval(timerInterval);
             alert("Time's up!");
         } else {
-            let minutes = Math.floor(timeLeft / 60);
-            let seconds = timeLeft % 60;
+            const minutes = Math.floor(timeLeft / 60);
+            const seconds = timeLeft % 60;
             timerElem.innerText = `Time Remaining: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
             timeLeft--;
         }
@@ -115,7 +167,13 @@ function startTimer() {
 }
 
 document.getElementById('simulateAttack').addEventListener('click', simulateAttack);
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function () {
     openRandomSecurityAlert(); // Call this function to display a random security alert when the page loads
     startTimer();
+
+    // Add the event listener for the "Change Password" button
+    document.getElementById('changePasswordButton').addEventListener('click', function () {
+        changePassword();
+    });
 });
